1b363d606164277f58efe256be730adfd209bd8d
Judy Lipinki <judy@karmoxie.com>
2017-04-07 04:16:08 -0400

    updated fro istanbul and new port for demos in middelware

--- Demos/Ch07-UsingExpress/4-middleware/serverMiddleware.js
+++ Demos/Ch07-UsingExpress/4-middleware/serverMiddleware.js
@@ -1,20 +1,26 @@
 const express = require('express')  
-const app = express()
+const app = express();
 
+//log request handlers
 app.use((request, response, next) => {  
-  console.log(request.headers)
-  next()
+  console.log(request.headers);
+  next();
 })
 
+//generate random number
 app.use((request, response, next) => {  
-  request.chance = Math.random()
-  next()
+  request.chance = Math.random();
+  next();
 })
 
+//put chance into response
 app.get('/', (request, response) => {  
   response.json({
     chance: request.chance
   })
 })
 
-app.listen(3000)  
+app.listen(3002, () => {
+	console.log(`Listening on port 3002...`);
+});
+
